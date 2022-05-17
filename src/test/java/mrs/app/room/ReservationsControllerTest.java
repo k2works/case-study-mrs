28206fa001ba730778e68521293f18e023610021
k2works <kakimomokkuri@gmail.com>
2022-05-17 16:45:57 +0900

    test: 会議室予約機能の実装

---
+++ src/test/java/mrs/app/room/ReservationsControllerTest.java
@@ -0,0 +1,46 @@
+package mrs.app.room;
+
+import mrs.app.reservation.ReservationsController;
+import mrs.domain.service.reservation.ReservationService;
+import mrs.domain.service.room.RoomService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.time.LocalDate;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@ActiveProfiles("dev")
+public class ReservationsControllerTest {
+    MockMvc mockMvc;
+
+    @InjectMocks
+    ReservationsController controller;
+
+    @Mock
+    RoomService mockRoomService;
+
+    @Mock
+    ReservationService reservationService;
+
+    @BeforeEach
+    public void setUpMockMvc() {
+        this.mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+    }
+
+    @Test
+    void 予約を表示する() throws Exception {
+        LocalDate today = LocalDate.now();
+        mockMvc.perform(get("/reservations/" + today.toString() + "/1"))
+                .andExpect(status().isOk());
+    }
+
+}
