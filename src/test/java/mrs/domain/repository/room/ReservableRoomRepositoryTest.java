cb70f78d49b1bc14a086df128d2c1fb9078001a0
k2works <kakimomokkuri@gmail.com>
2022-05-17 12:25:27 +0900

    test: 会議室一覧表示機能の実装

---
+++ src/test/java/mrs/domain/repository/room/ReservableRoomRepositoryTest.java
@@ -0,0 +1,29 @@
+package mrs.domain.repository.room;
+
+import mrs.domain.model.ReservableRoom;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
+import static org.hibernate.validator.internal.util.Contracts.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@DataJpaTest
+public class ReservableRoomRepositoryTest {
+
+    @Autowired
+    private ReservableRoomRepository reservableRoomRepository;
+
+    @Test
+    public void 会議室一覧を取得する() {
+        LocalDate date = LocalDate.now();
+        List<ReservableRoom> rooms = reservableRoomRepository.findByReservableRoomId_reservedDateOrderByReservableRoomId_roomIdAsc(date);
+        assertNotNull(rooms);
+        assertEquals(2, rooms.size());
+        assertEquals(java.util.Optional.ofNullable(rooms.get(1).getMeetingRoom().getRoomName()), Optional.of("有楽町"));
+    }
+}
