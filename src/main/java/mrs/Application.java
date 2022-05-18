3bb89bfb1cd8635baf98a58c6936f0e533c9c61c
k2works <kakimomokkuri@gmail.com>
2022-05-18 14:05:56 +0900

    refactor: 不要なコードの削除

--- src/main/java/mrs/Application.java
+++ src/main/java/mrs/Application.java
@@ -2,18 +2,11 @@ package mrs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class Application {
-
-	@RequestMapping("/")
-	String hello() {
-		return "Hello World!";
-	}
-
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
