8175a38fe80601f64ab2b281bcd76d6b1fffde5e
k2works <kakimomokkuri@gmail.com>
2022-05-19 12:00:10 +0900

    chore: PostgresSQLセットアップ

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -3,20 +3,34 @@ DROP TABLE IF EXISTS reservable_room CASCADE;
 DROP TABLE IF EXISTS reservation CASCADE;
 DROP TABLE IF EXISTS usr CASCADE;
 
-CREATE TABLE meeting_room
+CREATE TABLE public.meeting_room
 (
-    room_id   INT4         NOT NULL AUTO_INCREMENT,
+    room_id   SERIAL       NOT NULL,
     room_name VARCHAR(255) NOT NULL,
     PRIMARY KEY (room_id)
 );
-CREATE TABLE reservable_room
+comment
+on table public.meeting_room is '会議室';
+comment
+on column public.meeting_room.room_id is '会議室ID';
+comment
+on column public.meeting_room.room_name is '会議室名';
+
+CREATE TABLE public.reservable_room
 (
     reserved_date DATE NOT NULL,
     room_id       INT4 NOT NULL,
     PRIMARY KEY (reserved_date, room_id),
     FOREIGN KEY (room_id) REFERENCES meeting_room
 );
-CREATE TABLE usr
+comment
+on table public.reservable_room is '予約可能会議室';
+comment
+on column public.reservable_room.reserved_date is '予約日';
+comment
+on column public.reservable_room.room_id is '会議室ID';
+
+CREATE TABLE public.usr
 (
     user_id    VARCHAR(255) NOT NULL,
     first_name VARCHAR(255) NOT NULL,
@@ -25,9 +39,22 @@ CREATE TABLE usr
     role_name  VARCHAR(255) NOT NULL,
     PRIMARY KEY (user_id)
 );
-CREATE TABLE reservation
+comment
+on table public.usr is 'ユーザー';
+comment
+on column public.usr.user_id is 'ユーザーID';
+comment
+on column public.usr.first_name is '姓';
+comment
+on column public.usr.last_name is '名';
+comment
+on column public.usr.password is 'パスワード';
+comment
+on column public.usr.role_name is '役割';
+
+CREATE TABLE public.reservation
 (
-    reservation_id INT4         NOT NULL AUTO_INCREMENT,
+    reservation_id SERIAL       NOT NULL,
     end_time       TIME         NOT NULL,
     start_time     TIME         NOT NULL,
     reserved_date  DATE         NOT NULL,
@@ -37,3 +64,17 @@ CREATE TABLE reservation
     FOREIGN KEY (reserved_date, room_id) REFERENCES reservable_room,
     FOREIGN KEY (user_id) REFERENCES usr
 );
+comment
+on table public.reservation is '予約';
+comment
+on column public.reservation.reservation_id is '予約ID';
+comment
+on column public.reservation.end_time is '予約終了時間';
+comment
+on column public.reservation.start_time is '予約開始時間';
+comment
+on column public.reservation.reserved_date is '予約日';
+comment
+on column public.reservation.room_id is '会議室ID';
+comment
+on column public.reservation.user_id is 'ユーザーID';
