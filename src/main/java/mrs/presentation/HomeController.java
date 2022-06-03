2fd898b877b2341149e93659e83386d397fa98ca
k2works <kakimomokkuri@gmail.com>
2022-06-03 13:32:38 +0900

    style: 6-1 コンテンツページを作成する

--- src/main/java/mrs/presentation/HomeController.java
+++ src/main/java/mrs/presentation/HomeController.java
@@ -11,4 +11,9 @@ public class HomeController {
     public String index() {
         return "index";
     }
+
+    @RequestMapping("/content")
+    public String contents() {
+        return "content";
+    }
 }
