e18541f2e68839d22589d9a6051ed894bf2a2f45
k2works <kakimomokkuri@gmail.com>
2022-06-10 17:23:28 +0900

    test: 予約コンテキスト

---
+++ src/test/java/mrs/domain/model/reservation/ReservationTest.java
@@ -0,0 +1,33 @@
+package mrs.domain.model.reservation;
+
+import mrs.domain.model.auth.user.RoleName;
+import mrs.domain.model.auth.user.User;
+import mrs.domain.model.reservation.reservation.Reservation;
+import mrs.domain.model.reservation.room.ReservableRoomId;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ReservationTest {
+    @Test
+    void 予約を生成できる() {
+        User user = new User("1", "テスト", "太郎", "password", RoleName.USER);
+        ReservableRoomId reservableRoomId = new ReservableRoomId(1, LocalDate.of(2020, 1, 1));
+        Reservation reservation = new Reservation(
+                1,
+                LocalTime.of(9, 0),
+                LocalTime.of(10, 0),
+                reservableRoomId,
+                user
+        );
+
+        assertEquals(1, reservation.ReservationId().Value());
+        assertEquals(LocalTime.of(9, 0), reservation.ReservedTime().StartTime());
+        assertEquals(LocalTime.of(10, 0), reservation.ReservedTime().EndTime());
+        assertEquals(reservableRoomId, reservation.ReservableRoom().ReservableRoomId());
+        assertEquals(user, reservation.User());
+    }
+}
