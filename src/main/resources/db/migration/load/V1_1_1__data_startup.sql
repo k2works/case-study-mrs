9112a203026444af34dd06a9568305e511366c08
k2works <kakimomokkuri@gmail.com>
2022-06-17 18:32:53 +0900

    chore: 負荷テスト環境セットアップ

--- src/main/resources/db/migration/load/V1_1_1__data_startup.sql
+++ src/main/resources/db/migration/load/V1_1_1__data_startup.sql
@@ -1,18 +1,27 @@
+DELETE
+FROM reservation.予約;
+DELETE
+FROM reservation.予約可能会議室;
+DELETE
+FROM facility.会議室;
+DELETE
+FROM auth.利用者;
+
 -- 会議室
-INSERT INTO facility.会議室 (会議室名)
-VALUES ('新木場');
-INSERT INTO facility.会議室 (会議室名)
-VALUES ('辰巳');
-INSERT INTO facility.会議室 (会議室名)
-VALUES ('豊洲');
-INSERT INTO facility.会議室 (会議室名)
-VALUES ('月島');
-INSERT INTO facility.会議室 (会議室名)
-VALUES ('新富町');
-INSERT INTO facility.会議室 (会議室名)
-VALUES ('銀座一丁目');
-INSERT INTO facility.会議室 (会議室名)
-VALUES ('有楽町');
+INSERT INTO facility.会議室 (会議室番号, 会議室名)
+VALUES (1, '新木場');
+INSERT INTO facility.会議室 (会議室番号, 会議室名)
+VALUES (2, '辰巳');
+INSERT INTO facility.会議室 (会議室番号, 会議室名)
+VALUES (3, '豊洲');
+INSERT INTO facility.会議室 (会議室番号, 会議室名)
+VALUES (4, '月島');
+INSERT INTO facility.会議室 (会議室番号, 会議室名)
+VALUES (5, '新富町');
+INSERT INTO facility.会議室 (会議室番号, 会議室名)
+VALUES (6, '銀座一丁目');
+INSERT INTO facility.会議室 (会議室番号, 会議室名)
+VALUES (7, '有楽町');
 
 -- 会議室の予約可能日(room_idが2〜6用のSQLは省略
 -- room_id=1(新木場)の予約可能日
@@ -22,13 +31,6 @@ INSERT INTO reservation.予約可能会議室(予約日, 会議室番号)
 VALUES (CURRENT_DATE + 1, 1);
 INSERT INTO reservation.予約可能会議室(予約日, 会議室番号)
 VALUES (CURRENT_DATE - 1, 1);
--- room_id=7(有楽町)の予約可能日
-INSERT INTO reservation.予約可能会議室(予約日, 会議室番号)
-VALUES (CURRENT_DATE, 7);
-INSERT INTO reservation.予約可能会議室(予約日, 会議室番号)
-VALUES (CURRENT_DATE + 1, 7);
-INSERT INTO reservation.予約可能会議室(予約日, 会議室番号)
-VALUES (CURRENT_DATE - 1, 7);
 
 -- 認証確認用のテストユーザー(password = demo)
 INSERT INTO auth.利用者 (利用者番号, 姓, 名, パスワード, 役割)
@@ -44,3 +46,11 @@ VALUES ('U000004', '山田', '太郎', '$2y$10$aYjdzuGAep5SXGVBxxGrMOnh1b1mL62sJ
 -- 予約
 INSERT INTO reservation.予約 (予約終了時間, 予約開始時間, 予約日, 会議室番号, 利用者番号)
 VALUES ('9:00', '10:00', CURRENT_DATE, 1, 'U000001');
+INSERT INTO reservation.予約 (予約終了時間, 予約開始時間, 予約日, 会議室番号, 利用者番号)
+VALUES ('10:00', '11:00', CURRENT_DATE, 1, 'U000001');
+INSERT INTO reservation.予約 (予約終了時間, 予約開始時間, 予約日, 会議室番号, 利用者番号)
+VALUES ('11:00', '12:00', CURRENT_DATE, 1, 'U000001');
+INSERT INTO reservation.予約 (予約終了時間, 予約開始時間, 予約日, 会議室番号, 利用者番号)
+VALUES ('13:00', '14:00', CURRENT_DATE, 1, 'U000001');
+INSERT INTO reservation.予約 (予約終了時間, 予約開始時間, 予約日, 会議室番号, 利用者番号)
+VALUES ('14:00', '15:00', CURRENT_DATE, 1, 'U000001');
