94d49907040e9fb0ebc821f70d857ea6be7d11d1
k2works <kakimomokkuri@gmail.com>
2022-06-30 12:26:27 +0900

    STEP 4-5 記事を追加する

--- src/main/typescript/components/MainComponent.tsx
+++ src/main/typescript/components/MainComponent.tsx
@@ -4,6 +4,11 @@ import Logo from "../static/img/logo.svg";
 import Tool from "../static/img/tool.jpg";
 import Helpful from "../static/img/helpful.jpg";
 import News01 from "../static/img/news01.jpg";
+import News02 from "../static/img/news02.jpg";
+import News03 from "../static/img/news03.jpg";
+import News04 from "../static/img/news04.jpg";
+import News05 from "../static/img/news05.jpg";
+import News06 from "../static/img/news06.jpg";
 
 export const Main: React.FC<{}> = () => {
     return (
@@ -78,6 +83,65 @@ export const Main: React.FC<{}> = () => {
                                 <h3>スパンコール</h3>
                                 <p>
                                     キラキラと光を反射する装飾素材です。いつもの道具にアクセントを付けます。
+                                </p>
+                            </a>
+                        </article>
+
+
+                        <article className="post">
+                            <a href="#">
+                                <figure>
+                                    <img src={News02} alt="" width={1000} height={750}/>
+                                </figure>
+                                <h3>卓上ランタン</h3>
+                                <p>
+                                    手元に灯りが欲しいときに、便利でかわいい卓上ランタンです。
+                                </p>
+                            </a>
+                        </article>
+
+                        <article className="post">
+                            <a href="#">
+                                <figure>
+                                    <img src={News03} alt="" width={1000} height={750}/>
+                                </figure>
+                                <h3>冊子印刷</h3>
+                                <p>スタンダードプランにも手軽な冊子印刷が登場しました。</p>
+                            </a>
+                        </article>
+
+                        <article className="post">
+                            <a href="#">
+                                <figure>
+                                    <img src={News04} alt="" width={1000} height={750}/>
+                                </figure>
+                                <h3>マグネット</h3>
+                                <p>
+                                    メモやアイデアを貼り出しておくなら、かわいいマグネットがおすすめです。
+                                </p>
+                            </a>
+                        </article>
+
+                        <article className="post">
+                            <a href="#">
+                                <figure>
+                                    <img src={News05} alt="" width={1000} height={750}/>
+                                </figure>
+                                <h3>ビジネスノート</h3>
+                                <p>
+                                    書き心地や開きさすさにこだわった、ビジネス用のノートたちを紹介します。
+                                </p>
+                            </a>
+                        </article>
+
+                        <article className="post">
+                            <a href="#">
+                                <figure>
+                                    <img src={News06} alt="" width={1000} height={750}/>
+                                </figure>
+                                <h3>ボールペン</h3>
+                                <p>
+                                    スタイリッシュなボールペンで仕事環境の雰囲気を変えてみました。
                                 </p>
                             </a>
                         </article>
