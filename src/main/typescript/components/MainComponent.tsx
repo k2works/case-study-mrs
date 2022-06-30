d3656834e644256c61af91a86a95e533b28625f0
k2works <kakimomokkuri@gmail.com>
2022-06-30 11:16:47 +0900

    STEP2-1 ヒーローをマークアップする

--- src/main/typescript/components/MainComponent.tsx
+++ src/main/typescript/components/MainComponent.tsx
@@ -4,19 +4,29 @@ import Logo from "../static/img/logo.svg";
 
 export const Main: React.FC<{}> = () => {
     return (
-        <header className="header">
-            <div className="header-container w-container">
-                <div className="site">
-                    <a href="/">
-                        <img src={Logo} alt="" width={135} height={26}/>
-                    </a>
-                </div>
+        <div>
+            <header className="header">
+                <div className="header-container w-container">
+                    <div className="site">
+                        <a href="/">
+                            <img src={Logo} alt="" width={135} height={26}/>
+                        </a>
+                    </div>
 
-                <button className="navbtn">
-                    <i className="fas fa-bars"></i>
-                    <span className="sr-only">MENU</span>
-                </button>
-            </div>
-        </header>
+                    <button className="navbtn">
+                        <i className="fas fa-bars"></i>
+                        <span className="sr-only">MENU</span>
+                    </button>
+                </div>
+            </header>
+
+            <section className="hero">
+                <div className="hero-container w-container">
+                    <h1>Stationery Service</h1>
+                    <p>便利な道具とサービスをお届けします</p>
+                    <a href="#">無料で始める</a>
+                </div>
+            </section>
+        </div>
     );
 };
