e484e09ffdb840fb788b4e79ef18111438ee55a8
k2works <kakimomokkuri@gmail.com>
2022-06-30 11:37:24 +0900

    STEP 2-6 ボタンの表示を整える

--- src/main/typescript/components/MainComponent.tsx
+++ src/main/typescript/components/MainComponent.tsx
@@ -24,7 +24,9 @@ export const Main: React.FC<{}> = () => {
                 <div className="hero-container w-container">
                     <h1>Stationery Service</h1>
                     <p>便利な道具とサービスをお届けします</p>
-                    <a href="#">無料で始める</a>
+                    <a href="#" className="btn">
+                        無料で始める
+                    </a>
                 </div>
             </section>
         </div>
