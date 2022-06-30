2acf75348e1ef7ef88acb5432926b763900b70dc
k2works <kakimomokkuri@gmail.com>
2022-06-30 13:36:36 +0900

    STEP 6-1 コンテンツページを作成する

--- src/main/typescript/App.tsx
+++ src/main/typescript/App.tsx
@@ -2,12 +2,14 @@ import * as React from "react";
 import {Route, Routes} from "react-router-dom";
 import "./static/css/style.scss";
 import {Main} from "./components/MainComponent";
+import {Content} from "./components/ContentComponent";
 
 const App: React.FC = () => {
     return (
         <div>
             <Routes>
                 <Route path="/" element={<Main/>}/>
+                <Route path="/content" element={<Content/>}/>
             </Routes>
         </div>
     );
---
+++ src/main/typescript/components/ContentComponent.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import "../static/css/style.scss";
+import Logo from "../static/img/logo.svg";
+
+export const Content: React.FC<{}> = () => {
+    return (
+        <div>
+            <header className="header">
+                <div className="header-container w-container">
+                    <div className="site">
+                        <a href="/">
+                            <img src={Logo} alt="" width={135} height={26}/>
+                        </a>
+                    </div>
+
+                    <button className="navbtn">
+                        <i className="fas fa-bars"></i>
+                        <span className="sr-only">MENU</span>
+                    </button>
+                </div>
+            </header>
+        </div>
+    );
+};
