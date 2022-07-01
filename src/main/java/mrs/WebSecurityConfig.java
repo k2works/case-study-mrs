5488e5d64041fb4888729b395350501089f30f38
k2works <kakimomokkuri@gmail.com>
2022-07-01 14:14:36 +0900

    chore: セキュリティ設定分割

--- src/main/java/mrs/WebSecurityConfig.java
+++ src/main/java/mrs/WebSecurityConfig.java
@@ -3,6 +3,7 @@ package mrs;
 import mrs.application.service.auth.UserAuthService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -11,41 +12,54 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-@Configuration
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-    private final UserAuthService userDetailsService;
 
-    public WebSecurityConfig(UserAuthService userDetailsService) {
-        this.userDetailsService = userDetailsService;
+    @Configuration
+    @Order(1)
+    @EnableGlobalMethodSecurity(prePostEnabled = true)
+    public static class WebAppSecurityConfig extends WebSecurityConfigurerAdapter {
+        private final UserAuthService userDetailsService;
+
+        public WebAppSecurityConfig(UserAuthService userDetailsService) {
+            this.userDetailsService = userDetailsService;
+        }
+
+        @Bean
+        PasswordEncoder passwordEncoder() {
+            return new BCryptPasswordEncoder();
+        }
+
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http.authorizeRequests()
+                    .antMatchers("/js/**", "/css/**", "/img/**").permitAll()
+                    .antMatchers("/", "/content", "/contacts/gust/**").permitAll()
+                    .antMatchers("/**").authenticated()
+                    .and()
+                    .formLogin()
+                    .loginPage("/loginForm")
+                    .loginProcessingUrl("/login")
+                    .usernameParameter("username")
+                    .passwordParameter("password")
+                    .defaultSuccessUrl("/rooms", true)
+                    .failureUrl("/loginForm?error=true").permitAll().and()
+                    .exceptionHandling().accessDeniedPage("/accessDenied");
+            http.logout().logoutUrl("/logout").logoutSuccessUrl("/loginForm");
+        }
+
+        @Override
+        protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+            auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+        }
     }
 
-    @Bean
-    PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .antMatchers("/js/**", "/css/**", "/img/**").permitAll()
-                .antMatchers("/", "/content", "/contacts/gust/**").permitAll()
-                .antMatchers("/**").authenticated()
-                .and()
-                .formLogin()
-                .loginPage("/loginForm")
-                .loginProcessingUrl("/login")
-                .usernameParameter("username")
-                .passwordParameter("password")
-                .defaultSuccessUrl("/rooms", true)
-                .failureUrl("/loginForm?error=true").permitAll().and()
-                .exceptionHandling().accessDeniedPage("/accessDenied");
-        http.logout().logoutUrl("/logout").logoutSuccessUrl("/loginForm");
-    }
-
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+    @Configuration
+    @Order(2)
+    public static class ApiWebSecurityConfig extends WebSecurityConfigurerAdapter {
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http.antMatcher("/api/**");
+        }
     }
 }
