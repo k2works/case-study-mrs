760a59c4ce2aa19e994a8db5dd89a9344766c1ec
k2works <kakimomokkuri@gmail.com>
2022-07-01 14:27:52 +0900

    chore: セキュリティ設定環境分割

--- src/main/java/mrs/WebSecurityConfig.java
+++ src/main/java/mrs/WebSecurityConfig.java
@@ -3,6 +3,7 @@ package mrs;
 import mrs.application.service.auth.UserAuthService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -16,6 +17,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Configuration
+    @Profile("!test")
     @Order(1)
     @EnableGlobalMethodSecurity(prePostEnabled = true)
     public static class WebAppSecurityConfig extends WebSecurityConfigurerAdapter {
@@ -33,10 +35,12 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         @Override
         protected void configure(HttpSecurity http) throws Exception {
             http.authorizeRequests()
+                    // 認可
                     .antMatchers("/js/**", "/css/**", "/img/**").permitAll()
                     .antMatchers("/", "/content", "/contacts/gust/**").permitAll()
                     .antMatchers("/**").authenticated()
                     .and()
+                    // 認証
                     .formLogin()
                     .loginPage("/loginForm")
                     .loginProcessingUrl("/login")
@@ -62,4 +66,53 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
             http.antMatcher("/api/**");
         }
     }
+
+    @Configuration
+    @Profile("test")
+    @Order(3)
+    @EnableGlobalMethodSecurity(prePostEnabled = true)
+    public static class WebAppTestSecurityConfig extends WebSecurityConfigurerAdapter {
+        private final UserAuthService userDetailsService;
+
+        public WebAppTestSecurityConfig(UserAuthService userDetailsService) {
+            this.userDetailsService = userDetailsService;
+        }
+
+        @Bean
+        PasswordEncoder passwordEncoder() {
+            return new BCryptPasswordEncoder();
+        }
+
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http.authorizeRequests()
+                    // 認可
+                    .antMatchers("/js/**", "/css/**", "/img/**").permitAll()
+                    .antMatchers("/", "/content", "/contacts/gust/**").permitAll()
+                    .antMatchers("/h2-console/**").permitAll()
+                    .anyRequest().authenticated()
+                    .and()
+                    // 認証
+                    .formLogin()
+                    .loginPage("/loginForm")
+                    .loginProcessingUrl("/login")
+                    .usernameParameter("username")
+                    .passwordParameter("password")
+                    .defaultSuccessUrl("/rooms", true)
+                    .failureUrl("/loginForm?error=true").permitAll().and()
+                    .exceptionHandling().accessDeniedPage("/accessDenied")
+                    // For H2 Console
+                    .and()
+                    .headers().frameOptions().disable()
+                    .and()
+                    .csrf().disable();
+            http.logout().logoutUrl("/logout").logoutSuccessUrl("/loginForm");
+        }
+
+        @Override
+        protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+            auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+        }
+    }
+
 }
