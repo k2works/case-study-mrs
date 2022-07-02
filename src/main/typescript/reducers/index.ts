3586a47ae1499fc5d33d87f8e92208d6f5a1e8b8
k2works <kakimomokkuri@gmail.com>
2022-07-02 16:04:50 +0900

    test: 認証クライアント

--- src/main/typescript/reducers/index.ts
+++ src/main/typescript/reducers/index.ts
@@ -1,8 +1,12 @@
 import {combineReducers} from "redux";
 import counterReducer from "../features/counterSlice";
+import authReducer from '../features/auth/authSlice'
+import messageReducer from '../features/message/messageSlice'
 
 const rootReducer = combineReducers({
     counter: counterReducer,
+    auth: authReducer,
+    message: messageReducer
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
