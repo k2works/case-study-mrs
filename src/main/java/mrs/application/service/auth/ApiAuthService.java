f7ad2f3f7732a82391a2b2a5ae5f3acfc306c43c
k2works <kakimomokkuri@gmail.com>
2022-07-02 11:14:45 +0900

    refactor: クラスの抽出

---
+++ src/main/java/mrs/application/service/auth/ApiAuthService.java
@@ -0,0 +1,59 @@
+package mrs.application.service.auth;
+
+import mrs.domain.model.auth.user.*;
+import mrs.infrastructure.security.jwt.JwtUtils;
+import mrs.infrastructure.security.jwt.payload.response.JwtResponse;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * API認証
+ */
+@Service
+public class ApiAuthService {
+    final AuthenticationManager authenticationManager;
+
+    final UserRepository userRepository;
+
+    final PasswordEncoder passwordEncoder;
+
+    final JwtUtils jwtUtils;
+
+    public ApiAuthService(AuthenticationManager authenticationManager, UserRepository userRepository, PasswordEncoder passwordEncoder, JwtUtils jwtUtils) {
+        this.authenticationManager = authenticationManager;
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+        this.jwtUtils = jwtUtils;
+    }
+
+    public JwtResponse authenticateUser(UserId userId, Password password) {
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(userId.Value(), password.Value())
+        );
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        String jwt = jwtUtils.generateJwtToken(authentication);
+
+        UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
+        List<String> roles = userDetails.getAuthorities().stream()
+                .map(GrantedAuthority::getAuthority).toList();
+
+        return new JwtResponse(jwt, userDetails.getUsername(), roles);
+    }
+
+    public void registerUser(UserId userId, Password password, UserName userName, RoleName role) {
+        User newUser = new User(userId.Value(), userName.FirstName(), userName.LastName(), passwordEncoder.encode(password.Value()), role);
+        userRepository.save(newUser);
+    }
+
+    public Optional<User> checkUser(UserId userId) {
+        return userRepository.findByUserId(userId);
+    }
+}
--- src/main/java/mrs/presentation/api/auth/AuthController.java
+++ src/main/java/mrs/presentation/api/auth/AuthController.java
@@ -2,64 +2,43 @@ package mrs.presentation.api.auth;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import mrs.application.service.auth.UserRepository;
+import mrs.application.service.auth.ApiAuthService;
 import mrs.domain.model.auth.user.*;
-import mrs.infrastructure.security.jwt.JwtUtils;
 import mrs.infrastructure.security.jwt.payload.request.LoginRequest;
 import mrs.infrastructure.security.jwt.payload.request.SignupRequest;
 import mrs.infrastructure.security.jwt.payload.response.JwtResponse;
 import mrs.infrastructure.security.jwt.payload.response.MessageResponse;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import java.util.List;
 import java.util.Optional;
 
+/**
+ * 認証API
+ */
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/api/auth")
-@Tag(name = "JWTAuth", description = "JWT認証テスト")
+@Tag(name = "JWTAuth", description = "JWT認証")
 public class AuthController {
-    @Autowired
-    AuthenticationManager authenticationManager;
+    final ApiAuthService apiAuthService;
 
-    @Autowired
-    UserRepository userRepository;
-
-    @Autowired
-    PasswordEncoder passwordEncoder;
-
-    @Autowired
-    JwtUtils jwtUtils;
+    public AuthController(ApiAuthService apiAuthService) {
+        this.apiAuthService = apiAuthService;
+    }
 
     @Operation(summary = "ユーザー認証", description = "データベースに登録されているユーザーを認証する")
     @PostMapping("/signin")
     public ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginRequest loginRequest) {
         try {
-            Optional<User> user = userRepository.findByUserId(new UserId(loginRequest.getUserId()));
+            Optional<User> user = apiAuthService.checkUser(new UserId(loginRequest.getUserId()));
             if (user.isEmpty()) {
                 return ResponseEntity.badRequest().body(new MessageResponse("Error: User is not exist"));
             }
 
-            Authentication authentication = authenticationManager.authenticate(
-                    new UsernamePasswordAuthenticationToken(loginRequest.getUserId(), loginRequest.getPassword())
-            );
-            SecurityContextHolder.getContext().setAuthentication(authentication);
-            String jwt = jwtUtils.generateJwtToken(authentication);
-
-            UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
-            List<String> roles = userDetails.getAuthorities().stream()
-                    .map(GrantedAuthority::getAuthority).toList();
-
-            return ResponseEntity.ok(new JwtResponse(jwt, userDetails.getUsername(), roles));
+            JwtResponse jwtResponse = apiAuthService.authenticateUser(new UserId(loginRequest.getUserId()), new Password(loginRequest.getPassword()));
+            return ResponseEntity.ok(jwtResponse);
         } catch (Exception e) {
             return ResponseEntity.badRequest().body(new MessageResponse(e.getMessage()));
         }
@@ -69,23 +48,16 @@ public class AuthController {
     @PostMapping("/signup")
     public ResponseEntity<?> registerUser(@Valid @RequestBody SignupRequest signupRequest) {
         try {
-            Optional<User> user = userRepository.findByUserId(new UserId(signupRequest.getUserId()));
+            Optional<User> user = apiAuthService.checkUser(new UserId(signupRequest.getUserId()));
             if (user.isPresent()) {
                 return ResponseEntity.badRequest().body(new MessageResponse("Error: UserId is already taken"));
             }
 
             UserId userId = new UserId(signupRequest.getUserId());
-            String password = passwordEncoder.encode(signupRequest.getPassword());
+            Password password = new Password(signupRequest.getPassword());
             UserName userName = new UserName(signupRequest.getFirstName(), signupRequest.getLastName());
-            String role = signupRequest.getRole();
-            RoleName roleName;
-            if (role == null) {
-                roleName = RoleName.一般;
-            } else {
-                roleName = RoleName.valueOf(role);
-            }
-            User newUser = new User(userId.Value(), userName.FirstName(), userName.LastName(), password, roleName);
-            userRepository.save(newUser);
+            RoleName role = signupRequest.getRole() == null ? RoleName.一般 : RoleName.valueOf(signupRequest.getRole());
+            apiAuthService.registerUser(userId, password, userName, role);
 
             return ResponseEntity.ok(new MessageResponse("User registered successfully!"));
         } catch (Exception e) {
