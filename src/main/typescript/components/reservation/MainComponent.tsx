10444bab6b97e3b9980be3e81ddd84fcbe29bfdf
k2works <kakimomokkuri@gmail.com>
2022-07-04 16:03:50 +0900

    feat: 会議室の検索クライアント

--- src/main/typescript/components/reservation/MainComponent.tsx
+++ src/main/typescript/components/reservation/MainComponent.tsx
@@ -1,9 +1,63 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import "../../static/css/style.scss";
 import {AppHeader} from "../share/AppHeaderComponent";
 import {AppMenu} from "../share/AppMenuComponent";
+import {useNavigate} from "react-router-dom";
+import {TypedUseSelectorHook, useDispatch, useSelector} from "react-redux";
+import {AppDispatch, RootState} from "../../app/store";
+import {currentReservedDate, decrementReservedDate, roomList, roomState} from "../../features/room/roomSlice";
+import {clearMessage, selectMessage, setMessage} from "../../features/message/messageSlice";
 
 export const Main: React.FC<{}> = () => {
+    const navigate = useNavigate();
+    const dispatch = useDispatch<AppDispatch>();
+    const [successful, setSuccessful] = useState(false);
+
+    useEffect(() => {
+        dispatch(clearMessage());
+        list();
+    }, []);
+
+    const list = async () => {
+        setSuccessful(false);
+        const resultAction = await dispatch(roomList(new Date()));
+        if (roomList.fulfilled.match(resultAction)) {
+            dispatch(setMessage(resultAction.payload.message));
+            setSuccessful(true);
+        } else {
+            if (resultAction.payload) {
+                dispatch(setMessage(resultAction.payload.message));
+            } else {
+                dispatch(setMessage(resultAction.error.message));
+            }
+            setSuccessful(false);
+        }
+    }
+
+    const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+    const {message} = useAppSelector(selectMessage);
+    const room = useAppSelector(roomState);
+    const currentDay = useAppSelector(currentReservedDate);
+
+    const handlePreDay = async (e: any) => {
+        dispatch(decrementReservedDate(e.target.value));
+        const current = new Date(currentDay);
+        const preDay = new Date(current.setDate(current.getDate() - 1));
+        await dispatch(roomList(preDay));
+    }
+
+    const handleNextDay = async (e: any) => {
+        dispatch(decrementReservedDate(e.target.value));
+        const current = new Date(currentDay);
+        const nextDay = new Date(current.setDate(current.getDate() + 1));
+        await dispatch(roomList(nextDay));
+    }
+
+    const handleReservableRoom = (e: any) => {
+        const value = e.target.dataset["value"];
+        navigate(`/reservations?page=${value}`);
+    }
+
     return (
         <div>
             <AppHeader/>
@@ -16,19 +70,23 @@ export const Main: React.FC<{}> = () => {
                     </div>
 
                     <div className="app-decoration">
-                        <a href="/rooms/2022-06-29">&lt; 前日</a>
-                        <span>2022/6/30の会議室</span>
-                        <a href="/rooms/2022-07-01">翌日 &gt;</a>
+                        <a onClick={handlePreDay}>&lt; 前日</a>
+                        <span>{currentDay} の会議室</span>
+                        <a onClick={handleNextDay}>翌日 &gt;</a>
                     </div>
 
                     <div className="app-decoration">
                         <ul>
-                            <li>
-                                <a href="/reservations" id="No1">新木場</a>
-                            </li>
-                            <li>
-                                <a href="/reservations" id="No7">有楽町</a>
-                            </li>
+                            {
+                                room.reservableRooms.value.map(item => (
+                                    <li>
+                                        <a onClick={handleReservableRoom}
+                                           data-value={item.reservableRoomId.reservedDate.value}>
+                                            {item.meetingRoom.roomName.value}
+                                        </a>
+                                    </li>
+                                ))
+                            }
                         </ul>
                     </div>
                 </div>
