e948b30d9826d97486da3512c8515829cc973f72
k2works <kakimomokkuri@gmail.com>
2022-07-06 15:10:58 +0900

    chore: テストユーザー追加

--- src/main/resources/db/migration/e2e/V1_7_2__data_add.sql
+++ src/main/resources/db/migration/e2e/V1_7_2__data_add.sql
@@ -1,3 +1,4 @@
 INSERT INTO auth.利用者 (利用者番号, 姓, 名, パスワード, 役割)
 VALUES ('U000009', 'John', 'Doe', '$2a$10$aXH5nDjY1F4Vbzvhx7Xm3uHKazYwbZID.N.L.wM8CN2Ovj7jwEvfu', '一般');
-
+INSERT INTO auth.利用者 (利用者番号, 姓, 名, パスワード, 役割)
+VALUES ('U000010', 'Mr', 'X', '$2a$10$aXH5nDjY1F4Vbzvhx7Xm3uHKazYwbZID.N.L.wM8CN2Ovj7jwEvfu', '管理者');
