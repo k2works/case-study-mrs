39fbd1b07c079e6e26b29bc2f8f003688d7b26a4
k2works <kakimomokkuri@gmail.com>
2022-07-07 13:28:08 +0900

    feat: 予約可能会議室の登録クライアント

--- src/main/typescript/components/reservableRoom/MainComponent.tsx
+++ src/main/typescript/components/reservableRoom/MainComponent.tsx
@@ -1,8 +1,127 @@
-import React from "react";
+import React, {useEffect, useRef, useState} from "react";
 import {AppHeader} from "../share/AppHeaderComponent";
 import {AppMenu} from "../share/AppMenuComponent";
+import {useDispatch} from "react-redux";
+import {AppDispatch} from "../../app/store";
+import {useAppSelector} from "../../app/hook";
+import {clearMessage, selectMessage, setMessage} from "../../features/message/messageSlice";
+import {
+    reservableRoomCreate,
+    reservableRoomDelete,
+    reservableRoomList,
+    reservableRoomState
+} from "../../features/reservableRoom/reservableRoomSlice";
 
 export const Main: React.FC<{}> = () => {
+    const dispatch = useDispatch<AppDispatch>();
+    const [successful, setSuccessful] = useState(false);
+    const {message} = useAppSelector(selectMessage);
+    const reservableRoom = useAppSelector(reservableRoomState);
+    const [roomId, setRoomId] = useState(0);
+    const [reservedDate, setReservedDate] = useState("");
+    const registRef = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+        dispatch(clearMessage());
+        list().then(r => (setSuccessful(true)));
+    }, []);
+
+    const list = async () => {
+        setSuccessful(false);
+        const resultAction = await dispatch(reservableRoomList(1));
+        if (reservableRoomList.fulfilled.match(resultAction)) {
+            dispatch(setMessage(resultAction.payload.message));
+            setSuccessful(true);
+        } else {
+            if (resultAction.payload) {
+                dispatch(setMessage(resultAction.payload.message));
+            } else {
+                dispatch(setMessage(resultAction.error.message));
+            }
+            setSuccessful(false);
+        }
+    }
+
+    const handleRegistDialog = (e: any) => {
+        e.preventDefault();
+        setRoomId(0);
+        setReservedDate("");
+
+        if (registRef.current) {
+            registRef.current.style.left = ((window.innerWidth - 500) / 2) + 'px';
+            registRef.current.style.top = ((window.innerWidth - 500) / 2) + 'px';
+            registRef.current.style.display = 'block';
+        }
+    }
+
+    const handleClose = (e: any) => {
+        e.preventDefault();
+        if (registRef.current) {
+            registRef.current.style.display = 'none';
+        }
+    }
+
+    const handleReservedDate = (e: any) => {
+        setReservedDate(e.target.value);
+    }
+
+    const handleRoomId = (e: any) => {
+        setRoomId(e.target.value);
+    }
+
+    const formatRoomId = (id: number) => {
+        return id.toString().padStart(3, '0');
+    }
+
+    const handleRegist = async (e: any) => {
+        e.preventDefault();
+        setSuccessful(false);
+
+        const params = {
+            roomId,
+            reservedDate
+        }
+
+        const resultAction = await dispatch(reservableRoomCreate(params));
+        if (reservableRoomCreate.fulfilled.match(resultAction)) {
+            dispatch(setMessage(resultAction.payload.message));
+            setSuccessful(true);
+            await list();
+        } else {
+            if (resultAction.payload) {
+                dispatch(setMessage(resultAction.payload.message));
+            } else {
+                dispatch(setMessage(resultAction.error.message));
+            }
+            setSuccessful(false);
+        }
+    }
+
+    const handleDelete = async (e: any) => {
+        e.preventDefault();
+        setSuccessful(false);
+        const date = e.target.dataset["date"];
+        const id = e.target.dataset["id"];
+
+        const params = {
+            roomId: id,
+            reservedDate: date
+        }
+
+        const resultAction = await dispatch(reservableRoomDelete(params));
+        if (reservableRoomDelete.fulfilled.match(resultAction)) {
+            dispatch(setMessage(resultAction.payload.message));
+            setSuccessful(true);
+            await list();
+        } else {
+            if (resultAction.payload) {
+                dispatch(setMessage(resultAction.payload.message));
+            } else {
+                dispatch(setMessage(resultAction.error.message));
+            }
+            setSuccessful(false);
+        }
+    }
     return (
         <div>
             <AppHeader/>
@@ -16,13 +135,12 @@ export const Main: React.FC<{}> = () => {
 
                     <div className="app-decoration">
                         <div className="message">
-
-
+                            {!successful && (<p className="error">{message}</p>)}
                         </div>
                     </div>
 
                     <div className="app-decoration">
-                        <button className="regist app-btn" id="regist_button" type="submit">新規</button>
+                        <button className="regist app-btn" id="regist_button" onClick={handleRegistDialog}>新規</button>
                     </div>
 
                     <div className="app-decoration">
@@ -33,67 +151,44 @@ export const Main: React.FC<{}> = () => {
                                 <th>会議室番号</th>
                                 <th>会議室名</th>
                             </tr>
-                            <tr>
-                                <td>
-                                    <span>2022-07-07</span></td>
-                                <td>
-                                    <span>001</span></td>
-                                <td>
-                                    <span>新木場</span></td>
-                                <td>
-                                    <a className="app-btn app-btn-accent"
-                                       href="/reservableRooms/delete/1/2022-07-07">削除</a>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>
-                                    <span>2022-07-07</span></td>
-                                <td>
-                                    <span>007</span></td>
-                                <td>
-                                    <span>有楽町</span></td>
-                                <td>
-                                    <a className="app-btn app-btn-accent"
-                                       href="/reservableRooms/delete/7/2022-07-07">削除</a>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>
-                                    <span>2022-07-08</span></td>
-                                <td>
-                                    <span>001</span></td>
-                                <td>
-                                    <span>新木場</span></td>
-                                <td>
-                                    <a className="app-btn app-btn-accent"
-                                       href="/reservableRooms/delete/1/2022-07-08">削除</a>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td>
-                                    <span>2022-07-08</span></td>
-                                <td>
-                                    <span>007</span></td>
-                                <td>
-                                    <span>有楽町</span></td>
-                                <td>
-                                    <a className="app-btn app-btn-accent"
-                                       href="/reservableRooms/delete/7/2022-07-08">削除</a>
-                                </td>
-                            </tr>
+                            {
+                                reservableRoom.reservableRooms.list.map((item: any) => (
+                                    <tr>
+                                        <td>
+                                            <span>{item.reservableRoomId.reservedDate.value}</span>
+                                        </td>
+                                        <td>
+                                            <span>{formatRoomId(item.reservableRoomId.roomId.value)}</span>
+                                        </td>
+                                        <td>
+                                            <span>{item.meetingRoom.roomName}</span>
+                                        </td>
+                                        <td>
+                                            <button
+                                                className="app-btn app-btn-accent"
+                                                data-date={item.reservableRoomId.reservedDate.value}
+                                                data-id={item.reservableRoomId.roomId.value}
+                                                onClick={handleDelete}>
+                                                削除
+                                            </button>
+                                        </td>
+                                    </tr>
+                                ))
+                            }
                             </tbody>
                         </table>
 
-                        <div id="registDialog">
+                        <div id="registDialog" ref={registRef}>
                             <div>
-                                <form className="app-form" method="post" action="/reservableRooms">
+                                <form className="app-form">
                                     <label>予約日</label>
-                                    <input id="regist_reservedDate" name="reservedDate" type="date" value=""/>
+                                    <input id="regist_reservedDate" name="reservedDate" type="date" value={reservedDate}
+                                           onChange={handleReservedDate}/>
                                     <ul>
 
                                     </ul>
                                     <label>会議室</label>
-                                    <select id="regist_id" name="roomId">
+                                    <select id="regist_id" name="roomId" value={roomId} onChange={handleRoomId}>
                                         <option value="">---</option>
                                         <option value="1">新木場</option>
                                         <option value="2">辰巳</option>
@@ -106,8 +201,8 @@ export const Main: React.FC<{}> = () => {
                                     <ul>
 
                                     </ul>
-                                    <button className="app-btn" name="regist" type="submit">登録</button>
-                                    <button className="app-btn app-btn-accent" type="reset">キャンセル</button>
+                                    <button className="app-btn" name="regist" onClick={handleRegist}>登録</button>
+                                    <button className="app-btn app-btn-accent" onClick={handleClose}>キャンセル</button>
                                 </form>
                             </div>
                         </div>
