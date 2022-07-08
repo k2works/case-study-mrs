db91287fb4c3dc04eaa8dd4602e49dce65049b34
k2works <kakimomokkuri@gmail.com>
2022-07-08 17:24:43 +0900

    feat: 問い合わせ管理クライアント

--- src/main/typescript/components/contact/GusetContactComponent.tsx
+++ src/main/typescript/components/contact/GusetContactComponent.tsx
@@ -1,8 +1,52 @@
-import React from "react";
+import React, {useState} from "react";
 import "../../static/css/style.scss";
 import {Header} from "../home/HeaderComponent";
+import {useAppSelector} from "../../app/hook";
+import {useDispatch} from "react-redux";
+import {AppDispatch} from "../../app/store";
+import {selectMessage, setMessage} from "../../features/message/messageSlice";
+import {useForm} from "react-hook-form";
+import {contactCreate} from "../../features/contact/contactSlice";
+
+type FormData = {
+    details: string
+}
 
 export const GustContact: React.FC<{}> = () => {
+    const dispatch = useDispatch<AppDispatch>();
+    const [successful, setSuccessful] = useState(false);
+    const {message} = useAppSelector(selectMessage);
+    const {register, handleSubmit, formState: {errors}} = useForm<FormData>();
+    const [details, setDetails] = useState("");
+
+    const handleDetails = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+        setDetails(e.target.value);
+    }
+
+    const handleRegist = async (e: any) => {
+        setSuccessful(false);
+
+        const params = {
+            contactId: "",
+            details: details,
+            roleName: "",
+        }
+
+        const resultAction = await dispatch(contactCreate(params));
+        if (contactCreate.fulfilled.match(resultAction)) {
+            dispatch(setMessage(resultAction.payload.message));
+            dispatch(setMessage("問い合わせを送信しました"));
+            setSuccessful(true);
+        } else {
+            if (resultAction.payload) {
+                dispatch(setMessage(resultAction.payload.message));
+            } else {
+                dispatch(setMessage(resultAction.error.message));
+            }
+            setSuccessful(false);
+        }
+    }
+
     return (
         <div>
             <Header/>
@@ -11,22 +55,25 @@ export const GustContact: React.FC<{}> = () => {
                 <div className="app-container">
                     <div className="app-decoration">
                         <div className="message">
+                            {successful ? (<p className="success">{message}</p>) : (<p className="error">{message}</p>)}
                         </div>
                     </div>
 
                     <div className="app-decoration">
-                        <form className="app-form" method="post" action="/contacts/gust">
-                            <input type="hidden"
-                                   name="_csrf"
-                                   value="cd3137a5-cf48-4fc7-9026-d83855a7df96"/>
-
+                        <form className="app-form" onSubmit={
+                            handleSubmit(async (date) => {
+                                    await handleRegist(date);
+                                }
+                            )}>
                             <label>内容:</label>
-                            <textarea id="regist_details" name="details"></textarea>
+                            <textarea {...register("details", {required: true})} id="regist_details" name="details"
+                                      value={details}
+                                      onChange={handleDetails}></textarea>
                             <ul>
-
+                                {errors.details && <p className="error">内容を入力してください</p>}
                             </ul>
 
-                            <button className="app-btn" name="regist" type="submit">送信</button>
+                            <button className="app-btn" type="submit">送信</button>
                         </form>
                     </div>
                 </div>
