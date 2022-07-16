617c5ffbf97651b3e564ce7da62a6a5de4b17a3d
k2works <kakimomokuri@gmail.com>
2022-07-16 16:32:40 +0900

    fix: リンク

--- src/main/typescript/components/share/AppMenuComponent.tsx
+++ src/main/typescript/components/share/AppMenuComponent.tsx
@@ -2,11 +2,17 @@ import React from "react";
 import "../../static/css/style.scss";
 import {currentUser} from "../../features/auth/authSlice";
 import {useAppSelector} from "../../app/hook";
-import {Navigate} from "react-router-dom";
+import {Navigate, useNavigate} from "react-router-dom";
 
 export const AppMenu: React.FC<{}> = () => {
+    const navigate = useNavigate();
     const user = useAppSelector(currentUser);
     if (!user) return <Navigate to="/login"/>;
+
+    const handleOnClick = (e: any) => {
+        const href = e.target.dataset.href;
+        navigate(href);
+    }
 
     return (
         <div>
@@ -17,12 +23,11 @@ export const AppMenu: React.FC<{}> = () => {
                             <li className="menu-item a">
                                 <a href="">予約</a>
                                 <ul className="sub-menu">
-                                    <li className="sub-menu-item a"><a href="/rooms">会議室予約一覧</a></li>
+                                    <li className="sub-menu-item a"><a onClick={handleOnClick} data-href={"/rooms"} >会議室予約一覧</a></li>
                                     {user.userInfo?.roleName === "管理者" && (
                                         <>
-                                            <li className="sub-menu-item a"><a href="/meeting_rooms">会議室一覧</a></li>
-                                            <li className="sub-menu-item a"><a
-                                                href="/reservable_meeting_rooms">予約可能会議室一覧</a></li>
+                                            <li className="sub-menu-item a"><a  onClick={handleOnClick} data-href="/meeting_rooms">会議室一覧</a></li>
+                                            <li className="sub-menu-item a"><a  onClick={handleOnClick} data-href="/reservable_meeting_rooms">予約可能会議室一覧</a></li>
                                         </>
                                     )}
                                 </ul>
@@ -32,13 +37,13 @@ export const AppMenu: React.FC<{}> = () => {
                                 <ul className="sub-menu">
                                     {user.userInfo?.roleName === "管理者" && (
                                         <>
-                                            <li className="sub-menu-item b"><a href="/users">利用者一覧</a></li>
-                                            <li className="sub-menu-item b"><a href="/contacts">問い合わせ一覧</a></li>
+                                            <li className="sub-menu-item b"><a onClick={handleOnClick} data-href="/users">利用者一覧</a></li>
+                                            <li className="sub-menu-item b"><a onClick={handleOnClick} data-href="/contacts">問い合わせ一覧</a></li>
                                         </>
                                     )}
                                     {user.userInfo?.roleName === "一般" && (
                                         <>
-                                            <li className="sub-menu-item b"><a href="/contact_regist">問い合わせ</a></li>
+                                            <li className="sub-menu-item b"><a onClick={handleOnClick} data-href="/contacts_member_regist">問い合わせ</a></li>
                                         </>
                                     )}
                                 </ul>
