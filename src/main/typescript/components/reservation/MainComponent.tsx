a03c3c308cb47d407b7cd3ea472688e45e954efc
k2works <kakimomokkuri@gmail.com>
2022-08-19 16:44:46 +0900

    fix: 読み込み処理

--- src/main/typescript/components/reservation/MainComponent.tsx
+++ src/main/typescript/components/reservation/MainComponent.tsx
@@ -60,17 +60,21 @@ export const Main: React.FC<{}> = () => {
     const currentDay = useAppSelector(currentReservedDate);
 
     const handlePreDay = async (e: any) => {
+        setLoad(true);
         dispatch(decrementReservedDate(e.target.value));
         const current = new Date(currentDay);
         const preDay = new Date(current.setDate(current.getDate() - 1));
         await dispatch(roomList({reservedDate: preDay}));
+        setLoad(false);
     }
 
     const handleNextDay = async (e: any) => {
+        setLoad(true);
         dispatch(incrementReservedDate(e.target.value));
         const current = new Date(currentDay);
         const nextDay = new Date(current.setDate(current.getDate() + 1));
         await dispatch(roomList({reservedDate: nextDay}));
+        setLoad(false);
     }
 
     const handleReservableRoom = (e: any) => {
